3566cc2561caa40b2cf223582a4f43f11f1f0706
Yupeng Tang <charles.typ.xjtu@gmail.com>
2019-07-01 20:47:03 +0800

    add log

--- libjiffy/src/jiffy/storage/client/replica_chain_client.cpp
+++ libjiffy/src/jiffy/storage/client/replica_chain_client.cpp
@@ -93,6 +93,7 @@ std::vector<std::string> replica_chain_client::run_command(int32_t cmd_id, const
       }
     } catch (apache::thrift::transport::TTransportException &e) {
       LOG(log_level::info) << "Error in connection to chain: " << e.what();
+      LOG(log_level::info) << "Chain" << chain_.to_string();
       connect(fs_->resolve_failures(path_, chain_), timeout_ms_);
       retry = true;
     } catch (std::logic_error &e) {
