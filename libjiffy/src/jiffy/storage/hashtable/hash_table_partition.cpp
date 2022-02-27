fdcc6734e12abfae2195ee9a7ab49a85ea565ef8
Yupeng Tang <yupeng.tang@yale.edu>
2022-02-27 17:36:20 -0500

    Revert

--- libjiffy/src/jiffy/storage/hashtable/hash_table_partition.cpp
+++ libjiffy/src/jiffy/storage/hashtable/hash_table_partition.cpp
@@ -155,8 +155,8 @@ void hash_table_partition::get(response &_return, const arg_list &args) {
         if (metadata_ == "exporting" && in_export_slot_range(hash)) {
           RETURN_ERR("!exporting", export_target_str_);
         }
-        //RETURN_ERR("!key_not_found");
-        RETURN_OK(std::string("ok"));
+        RETURN_ERR("!key_not_found");
+        //RETURN_OK(std::string("ok"));
       }
     END_CATCH_HANDLER;
   }
